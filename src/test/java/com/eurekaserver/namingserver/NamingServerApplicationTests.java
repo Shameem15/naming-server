2fb8896e1791c0fe29a6b98574b210c412d95faf
Shameem Akhtar <shameem.akthar@ibm.com>
2022-01-02 16:01:17 +0530

    Added eureka server

---
+++ src/test/java/com/eurekaserver/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurekaserver.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
